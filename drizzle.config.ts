eb1642adcf867319c79335b049628d1484360fb3
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-05-09 20:52:39 +0300

    Removed check for database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) process.env.DATABASE_URL="";
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
