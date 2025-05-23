9fede6fd9969cc63f1e84bb41ef2317deaaca203
Michael Mukiibi <mikemukiibi2005@gmail.com>
2025-05-24 02:02:07 +0300

    Added GTM

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,17 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-FP37CJWGDR"></script>
+
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-FP37CJWGDR');
+	</script>
+</svelte:head>
+
 {@render children()}
